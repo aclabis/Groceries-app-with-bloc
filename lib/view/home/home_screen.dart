9d5540abf9b9da49ab53ebe104c913ff4b1b7835
labisac <labishchhetri345@gmail.com>
2024-08-24 09:12:11 +0545

    changes

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -44,9 +44,9 @@ class HomeScreen extends StatelessWidget {
                 )
               ],
             ),
-            
+             
             body: ListView.builder(
-              itemCount: products.length,
+              itemCount:  products.length,
               itemBuilder: (context, index) {
                 return ProductWidget(
                   productmodel: products[index],
